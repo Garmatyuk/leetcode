0a9eeb4d834d0462ba7e8ec6ef4098cf775dfea9
Pavel Garmatyuk <m-krum@yandex.ru>
2022-02-15 23:15:35 +0300

    13. Roman to Integer done

---
+++ src/main/java/leetcode0xx/RomantoInteger13/Solution.java
@@ -0,0 +1,78 @@
+package leetcode0xx.RomantoInteger13;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ * <p>
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+ * <p>
+ * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+ * <p>
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given a roman numeral, convert it to an integer.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "III"
+ * Output: 3
+ * Explanation: III = 3.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: s = "LVIII"
+ * Output: 58
+ * Explanation: L = 50, V= 5, III = 3.
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: s = "MCMXCIV"
+ * Output: 1994
+ * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ */
+class Solution {
+    public int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        Map<Character, Integer> romanValue = new HashMap<>();
+        romanValue.put('I', 1);
+        romanValue.put('V', 5);
+        romanValue.put('X', 10);
+        romanValue.put('L', 50);
+        romanValue.put('C', 100);
+        romanValue.put('D', 500);
+        romanValue.put('M', 1000);
+
+        int result = romanValue.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (romanValue.get(s.charAt(i)) >= romanValue.get(s.charAt(i + 1))) {
+                result += romanValue.get(s.charAt(i));
+            } else {
+                result -= romanValue.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.romanToInt("III"));
+        System.out.println(solution.romanToInt("LVIII"));
+        System.out.println(solution.romanToInt("MCMXCIV"));
+    }
+}
